fe46bb3f0439bee7b559197c224324a8072864e4
Vitor da Fonseca Rodrigues <vitorfon20@gmail.com>
2023-09-23 12:17:10 -0300

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -1,4 +1,4 @@
 /* 2: Liste os nomes dos supervisores dos empregados que ganham mais do que R$5.000.*/
-SELECT * FROM EMPREGADO AS e  JOIN empregado AS s ON s.cpf = e.superv
+SELECT S.ENOME AS SUPERnome, S.SALARIO, E.ENOME as EMPnome FROM EMPREGADO AS e  INNER JOIN empregado AS s ON s.cpf = e.superv
 WHERE s.salario>5000
 
