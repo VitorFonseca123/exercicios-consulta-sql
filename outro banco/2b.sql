ccfe722a4bdeda9e1a3c4a0f24c87d4e866fe8b5
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2023-12-07 18:24:59 -0300

    Create 2b.sql

---
+++ outro banco/2b.sql
@@ -0,0 +1,6 @@
+/*1) 2) Encontre o faturamento mensal da empresa, considerando que
+todos os imóveis estejam alugados. A coluna de saída da consulta
+dever ser nomeada como ‘Faturamento’.
+
+*/
+select sum(rent) as Faturamento from propertyforrent
